94bdd544991e316d9359303ddcbaa3db9178146e
marcbo <marcbo@student.ethz.ch>
2023-02-15 05:09:17 +0100

    fix config bug

--- set_configs.py
+++ set_configs.py
@@ -7,13 +7,16 @@ import sys
 modelpath = sys.argv[1]
 yamlpath = sys.argv[2]
 
-data_line = 91
+data_line = 90
 data_entry = "data_dir: "+modelpath+"\n"
-model_line = 92
+model_line = 91
 model_entry = "model_dir: "+modelpath + "/model\n"
 
-with open(yamlpath, "r+") as file:
+data = []
+
+with open(yamlpath, "r") as file:
     data = file.readlines()
     data[data_line] = data_entry
     data[model_line] = model_entry
-    file.writelines(data)
+with open(yamlpath, "w") as outfile:
+    outfile.writelines(data)
