c8f0ab7a762a427493fc19425ba85e482bf9ce8c
marcbo <marcbo@student.ethz.ch>
2023-04-01 18:33:45 +0200

    conda env changed

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 #Activate conda environment
 #source /home/marcbo/.bashrc
 [[ -f /itet-stor/${USER}/net_scratch/conda/bin/conda ]] && eval "$(/itet-stor/${USER}/net_scratch/conda/bin/conda shell.bash hook)"
-conda activate deepcom_env
+conda activate deepcomenv
 echo "Conda activated"
 
 
